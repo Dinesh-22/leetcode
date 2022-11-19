685f7b14f78fcc92b5dec88d9d39bade1663472f
Dinesh-22 <86187157+Dinesh-22@users.noreply.github.com>
2022-11-19 07:53:55 +0530

    Added solution - LeetHub

---
+++ Check if the door is open or closed - GFG/check-if-the-door-is-open-or-closed.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            int[] ptr = ob.checkDoorStatus(N);
+            for(int i=0 ; i<N ; i++)
+            {
+                System.out.print(ptr[i]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int[] checkDoorStatus(int N) {
+        // code here
+        int arr[]=new int[N];
+        for(int i=1;i<=N;i++)
+        {
+            if(Math.floor(Math.sqrt(i))==Math.ceil(Math.sqrt(i)))
+            {
+                arr[i-1]=1;
+            }
+            else
+            {
+                arr[i-1]=0;
+            }
+        }
+        return arr;
+        
+    }
+};
