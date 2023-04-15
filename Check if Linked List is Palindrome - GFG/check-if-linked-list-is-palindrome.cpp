46ea732f027fef296c712fc028d6908f66ad805a
Dinesh-22 <86187157+Dinesh-22@users.noreply.github.com>
2023-04-15 12:16:11 +0530

    Added solution - LeetHub

--- Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -32,28 +32,52 @@ struct Node {
 class Solution{
   public:
     //Function to check whether the list is palindrome.
-    bool checkPalindrome(vector<int> arr){
-        int n = arr.size();
-        int start = 0 , end = n-1;
-        while(start <= end){
-            if(arr[start]!=arr[end]){
-                return false;
-            }
-            start++;
-            end--;
+    Node* getMid(Node* head){
+        Node* slow = head;
+        Node* fast = head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        return true;
+        return slow;
+    }
+    Node* reverse(Node* head){
+        Node* curr = head;
+        Node* forward = NULL;
+        Node* prev = NULL;
+        while(curr!=NULL){
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
     }
     bool isPalindrome(Node *head)
     {
         //Your code here
-        vector<int> arr;
-        Node* temp = head;
-        while(temp!=NULL){
-            arr.push_back(temp->data);
-            temp = temp->next;
+        // find Middle of the Linked List 
+        Node* middle = getMid(head);
+        //reverse that part of the linked list 
+        Node* temp = middle->next;
+        middle->next=reverse(temp);
+        
+        Node* head1 = head;
+        Node* head2 = middle->next;
+        
+        //compare both the lists 
+        while(head2!=NULL){
+            if(head1->data != head2->data){
+                return false;
+            }
+            head1 = head1->next;
+            head2 = head2->next;
         }
-        return checkPalindrome(arr);
+        
+        temp = middle->next;
+        middle->next=reverse(temp);
+        
+        return true;
     }
 };
 
