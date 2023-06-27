4bb933b3d46cc4e9145256df94417872be493d71
Dinesh-22 <86187157+Dinesh-22@users.noreply.github.com>
2023-06-27 21:03:46 +0530

    Added solution - LeetHub

--- Combination Sum II - GFG/combination-sum-ii.cpp
+++ Combination Sum II - GFG/combination-sum-ii.cpp
@@ -1,73 +1,68 @@
 //{ Driver Code Starts
-// Initial Template for C++
+//Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // } Driver Code Ends
-// User function Template for C++
+//User function Template for C++
 
-class Solution {
-  public:
-    void f(int ind, int target, vector<int>&arr, vector<vector<int>>&ans, vector<int>&ds){
+class Solution{
+public:
+    void f(int ind,vector<int>&arr, int n, int target, vector<vector<int>>&ans,vector<int>&ds)
+    {
         if(target == 0){
             ans.push_back(ds);
             return;
         }
-        for(int i=ind;i<arr.size();i++){
-            if(i>ind && arr[i] == arr[i-1]) continue;
-            if(arr[i] > target) break;
+        for(int i=ind;i<arr.size();i++)
+        {
+            if(i>ind && arr[i]==arr[i-1])continue;
+            if(arr[i]>target) break;
             ds.push_back(arr[i]);
-            f(i+1,target-arr[i],arr,ans,ds);
+            f(i+1,arr,n,target-arr[i],ans,ds);
             ds.pop_back();
         }
     }
-    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
-        // Write your code here
+    vector<vector<int>> CombinationSum2(vector<int> arr,int n,int k)
+    {
+        //code here
+        sort(arr.begin(),arr.end());
         vector<vector<int>>ans;
         vector<int>ds;
-        sort(candidates.begin(),candidates.end());
-        f(0,target,candidates,ans,ds);
+        f(0,arr,n,k,ans,ds);
         return ans;
     }
 };
 
 //{ Driver Code Starts.
-int main() {
 
-    int t = 1;
-    cin >> t;
 
-    // freopen ("output_gfg.txt", "w", stdout);
-
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> candidates(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> candidates[i];
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
         }
-        // char marker;
-        // cin >> marker;
-
-        Solution obj;
-
-        vector<vector<int>> comb = obj.combinationSum2(candidates, k);
-        sort(comb.begin(), comb.end());
-        cout << "[ ";
-        for (int i = 0; i < comb.size(); i++) {
-            cout << "[ ";
-            for (int j = 0; j < comb[i].size(); j++) {
-                cout << comb[i][j] << " ";
+        Solution ob;
+        auto obj=ob.CombinationSum2(arr,n,k);
+        for(int i=0;i<obj.size();i++)
+        {
+            for(int j=0;j<obj[i].size();j++)
+            {
+                cout<<obj[i][j]<<" ";
             }
-            cout << "]";
+            cout<<"\n";
         }
-        cout << " ]\n";
-
-        // cout << "\n~\n";
+        if(obj.size()==0) cout<<"\n";
     }
-    fclose(stdout);
     return 0;
 }
-
 // } Driver Code Ends
